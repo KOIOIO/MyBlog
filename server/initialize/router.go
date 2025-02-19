5d3adae266878ce7cefe97ba28ee89f023f0da36
wang-wenyu-fdhfj <2652777599@qq.com>
2025-02-19 20:38:14 +0800

    jwt中间件及双token实现

--- server/initialize/router.go
+++ server/initialize/router.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 	"server/global"
+	"server/middleware"
 	"server/router"
 )
 
@@ -13,7 +14,9 @@ func InitRouter() *gin.Engine {
 	// 设置gin模式
 	gin.SetMode(global.Config.System.Env)
 	Router := gin.Default()
-
+	//使用日志记录中间键
+	Router.Use(middleware.GinLogger(), middleware.GinRecovery(true))
+	//使用gin会话路由
 	var store = cookie.NewStore([]byte(global.Config.System.SessionsSecret))
 	Router.Use(sessions.Sessions("session", store))
 
