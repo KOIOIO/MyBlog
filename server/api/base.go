134c018b6e9dd888829ac12126072174334c2649
wang-wenyu-fdhfj <2652777599@qq.com>
2025-02-19 19:46:00 +0800

    基础路由

--- server/api/base.go
+++ server/api/base.go
@@ -5,6 +5,8 @@ import (
 	"github.com/mojocn/base64Captcha"
 	"go.uber.org/zap"
 	"server/global"
+	"server/model/request"
+	"server/model/response"
 )
 
 type BaseApi struct {
@@ -25,6 +27,36 @@ func (baseApi *BaseApi) Captcha(c *gin.Context) {
 	id, b64s, _, err := captcha.Generate()
 	if err != nil {
 		global.Log.Error("验证码获取失败!", zap.Error(err))
-
+		response.FailWithMessage("Fail to genterate captcha", c)
+		return
 	}
+	response.OkWithData(response.Captcha{
+		CaptchaID: id,
+		PicPath:   b64s,
+	}, c)
+}
+
+func (baseApi *BaseApi) SendEmailVerificationCode(c *gin.Context) {
+	var req request.SendEmailVerificationCode
+	err := c.ShouldBindJSON(&req)
+	if err != nil {
+		response.FailWithMessage(err.Error(), c)
+		return
+	}
+	if store.Verify(req.CaptchaID, req.Captcha, true) {
+		err := baseService.SendEmailVerificationCode(c, req.Email)
+		if err != nil {
+			global.Log.Error("验证码发送失败!", zap.Error(err))
+			response.FailWithMessage("Fail to send email verification code", c)
+			return
+		}
+		response.OkWithMessage("Success to send email verification code", c)
+		return
+	}
+	response.FailWithMessage("Incorrect verification code", c)
+}
+
+func (baseApi *BaseApi) QQLoginURL(c *gin.Context) {
+	url := global.Config.QQ.QQLoginURL()
+	response.OkWithData(url, c)
 }
