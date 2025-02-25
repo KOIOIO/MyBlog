acb36b4db893e1dffb46e8464d8ac142ac5ac444
wang-wenyu-fdhfj <2652777599@qq.com>
2025-02-25 20:07:20 +0800

    跨域问题解决

--- web/src/views/web/index.vue
+++ web/src/views/web/index.vue
@@ -1,9 +1,18 @@
 <template>
-
+{{ url }}
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+import {qqLoginURL} from "@/api/base.ts";
+const usr=ref
+const getUrl = async ()=>{
+  const res =await qqLoginURL()
+  if(res.code===0){
+    url.value=res.data
+  }
+}
 </script>
-
+getUrl()
 <style scoped lang="scss">
 </style>
